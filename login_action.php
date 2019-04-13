9cd824ab5630bbabfa1b572132aa00cb011fa4a7
Jack Baumann <wavejbaumann@gmail.com>
2019-04-13 14:14:39 -0500

    Created login form, downloaded jQuery, updated footer

---
+++ login_action.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        Email entered: <?php echo $_POST["email"]; ?>
+        Password entered: <?php echo $_POST["password"]; ?>
+        Actor or Director? <?php echo $_POST["actorOrDirector"]; ?>
+    </body>
+</html>
