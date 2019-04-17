3fa32eee6e0ed6faad146db854690b9d422d49c5
Jack Baumann <wavejbaumann@gmail.com>
2019-04-17 13:26:11 -0500

    Fixed login form submission & post retrieval

--- login_action.php
+++
@@ -1,7 +0,0 @@
-<html>
-    <body>
-        Email entered: <?php echo $_POST["email"]; ?>
-        Password entered: <?php echo $_POST["password"]; ?>
-        Actor or Director? <?php echo $_POST["actorOrDirector"]; ?>
-    </body>
-</html>
