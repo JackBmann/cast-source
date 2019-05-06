17e9e618b3f414de4e5767b9c22d7dcc8865be3e
Jack Baumann <wavejbaumann@gmail.com>
2019-05-06 15:17:27 -0500

    Edited datepicker

--- js/javascript.js
+++ js/javascript.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.select').material_select();
-    $('.datepicker').pickadate();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year
+        format: 'dd-mm-yyyy'});
 });
